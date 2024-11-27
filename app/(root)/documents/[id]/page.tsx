d60e73f05cd31be9671828000e9d684869f1ef6a
gerard415 <chinedumike85@gmail.com>
2024-11-27 18:52:32 -0500

    clerk set up

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,22 @@
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div>
+          <p className='text-white'>Share</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
       <Editor/>
     </div>
   )
